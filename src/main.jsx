542073320c01309e970b42b885c47fea6e5854fd
vicvazq30-eng <vicvazq30@gmail.com>
2026-05-07 17:31:01 -0400

    Add main.jsx to render HQSCotizadorDemo component

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import HQSCotizadorDemo from "../hqs_cotizador_demo";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <HQSCotizadorDemo />
+  </React.StrictMode>
+);
